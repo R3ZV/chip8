13200c3ae181fe401bc5473abaa9b8d6ba091533
r3zv <razvan.maracine@protonmail.com>
2024-07-19 22:33:18 +0300

    Interactive CLI for ROM selection

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
 mod chip8;
 
+use inquire::{InquireError, Select};
 use macroquad::prelude::*;
+use std::fs;
 use std::time::{Duration, SystemTime};
+
+fn get_roms() -> Vec<String> {
+    let entries = fs::read_dir("roms").expect("No roms folder");
+    let mut roms = Vec::new();
+    for entry in entries {
+        let rom = entry.unwrap();
+        let rom = rom
+            .file_name()
+            .into_string()
+            .expect("Can't convert file name to String");
+        roms.push(rom);
+    }
+    roms
+}
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
-    let path = String::from("roms/corax.ch8");
-    let mut emulator = chip8::Chip8::new(path);
+    let options = get_roms();
+    let ans = Select::new("What ROM do you want to run?", options).prompt();
+    let selected_rom = ans.expect("No rom selected");
+
+    let rom_path = format!("roms/{selected_rom}");
+    let mut emulator = chip8::Chip8::new(rom_path);
 
     // Chip8 timer should be updated at a rate of 60hz
     // So will simulate that by decreasing the timer every 16.67ms
@@ -25,8 +45,8 @@ async fn main() {
         }
 
         emulator.start_cycle();
-
         emulator.update_screen();
+
         next_frame().await;
     }
 }
